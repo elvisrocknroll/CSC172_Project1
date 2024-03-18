f12d233ed2f8fc221afd0aa5b63c714fcd979b60
tiffany xiang <tiffany@macbook-pro.lan>
2024-03-17 21:31:37 -0400

    test

--- BinaryTools.java
+++ BinaryTools.java
@@ -1,20 +1,28 @@
-package CSC172_Project1;
 
-public class ReadWrite {
-public static String readFile(String filename) throws IOException {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class BinaryTools {
+    //test
+
+    public static String readFile(String filename) throws IOException {
         StringBuilder content = new StringBuilder();
-        BufferedReader reader = new BufferedReader(new FileReader(filename));
-        String line;
-        while ((line = reader.readLine()) != null) {
-            content.append(line);
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
+            }
         }
-        reader.close();
         return content.toString();
     }
 
     public static void writeFile(String filename, String content) throws IOException {
-        BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
-        writer.write(content);
-        writer.close();
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            writer.write(content);
+        }
     }
 }
+
